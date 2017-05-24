86a91714d50676596f852330efc6f123cfbdb4ca
Simon Struck <simonstruck70@gmail.com>
2017-05-24 19:03:31 +0200

    Added Server Sync

---
+++ .scripts/download.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rsync -uhvdEa --progress --delete simon@titan-systems.net:/home/simon/workspaces/ /home/simon/workspaces/
---
+++ .scripts/upload.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rsync -uhvdEa --progress --delete /home/simon/workspaces/ simon@titan-systems.net:/home/simon/workspaces/
